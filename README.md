cf3a224b0eeb87d5a27b5ecb895b6cc9d9664736
han <copetit412@gmail.com>
2022-04-27 20:39:32 +0900

    add readme

--- README.md
+++ README.md
@@ -9,11 +9,12 @@ website parts clone coding with Next.js and Tailwind CSS
 http://localhost:3000/
 
 #### Normal Screen
+
 <img width="847" alt="normal" src="https://user-images.githubusercontent.com/21287797/162182927-789f8125-75b4-49be-be96-7c0172771835.png">
 
 #### Dark Mode Screen
 
-Press the Dark mode btn. 
+Press the Dark mode btn.
 <img width="847" alt="darkmode" src="https://user-images.githubusercontent.com/21287797/162182917-1c662555-35df-4d59-b5aa-f99cbe221292.png">
 
 ### IKEA Item List
@@ -21,6 +22,12 @@ Press the Dark mode btn.
 http://localhost:3000/ikea
 
 <img width="847" alt="ikea_sample" src="https://user-images.githubusercontent.com/21287797/162174918-109e1a04-6fdd-4cd0-b46d-1e61cceb8658.png">
+
+### Login Form 　（using react-hook-form）
+
+http://localhost:3000/login
+
+<img width="613" alt="login" src="https://user-images.githubusercontent.com/21287797/165510159-c37a7fd9-457e-4994-8195-808576198eb9.png">
 
 ## Usage
 
@@ -36,6 +43,7 @@ Default port number is 3000. but depend on the your env, it can be changed.
 http://localhost:3000/
 
 ## Zenn Posting (JP)
+
 How to set up Next.js and tailwindCSS. also explain the source code in jp
 
 https://zenn.dev/redpanda/articles/296f82e9fb2727
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
 
 interface LoginForm {
@@ -23,12 +24,19 @@ const Login: NextPage = () => {
   return (
     <div className="mx-auto w-full max-w-lg">
       <div className="flex w-full flex-col items-center justify-center p-10">
-        <div className="h-24 w-24 rounded-full bg-gray-300" />
+        <Image
+          src="/cat.jpg"
+          width={130}
+          height={130}
+          layout="fixed"
+          className="rounded-full shadow-lg"
+          alt="profile"
+        />
         <form
           onSubmit={handleSubmit(isValid, isInValid)}
           className="flex w-full  flex-col items-center space-y-5 "
         >
-          <div className="flex w-full flex-col">
+          <div className="flex w-full flex-col space-y-2">
             <label className="text-sm text-gray-800" htmlFor="email">
               Email
             </label>
@@ -38,11 +46,30 @@ const Login: NextPage = () => {
               type="email"
               name="email"
             />
-            <div className="mt-1 font-semibold text-rose-500">
-              {errors.email?.message}
-            </div>
+            {errors.email && (
+              <div
+                className="flex rounded-lg bg-red-100 p-4 dark:bg-red-200"
+                role="alert"
+              >
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-red-700 dark:text-red-800"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <div className="mt-1 ml-3 text-sm font-medium text-red-700 dark:text-red-800">
+                  {errors.email.message}
+                </div>
+              </div>
+            )}
           </div>
-          <div className="flex w-full flex-col">
+          <div className="flex w-full flex-col space-y-2">
             <label className="text-sm text-gray-800" htmlFor="password">
               Password
             </label>
@@ -55,9 +82,28 @@ const Login: NextPage = () => {
               type="password"
               name="password"
             />
-            <div className="mt-1 font-semibold text-rose-500">
-              {errors.password?.message}
-            </div>
+            {errors.password && (
+              <div
+                className="flex rounded-lg bg-red-100 p-4 dark:bg-red-200"
+                role="alert"
+              >
+                <svg
+                  className="h-5 w-5 flex-shrink-0 text-red-700 dark:text-red-800"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <div className="ml-3 text-sm font-medium text-red-700 dark:text-red-800">
+                  {errors.password.message}
+                </div>
+              </div>
+            )}
           </div>
           <button
             className="w-full rounded-lg bg-teal-500 px-3 py-2 text-lg font-semibold text-white focus:outline-none"
